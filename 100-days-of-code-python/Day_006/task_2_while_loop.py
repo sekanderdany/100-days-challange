011d364fcdc0ff117f944ca5541d8cd192961c80
Sekander Dany <sekander.dany@reddotdigitalit.com>
2025-12-25 19:24:37 +0600

    adding go

--- 100-days-of-code-python/Day_006/task_2_while_loop.py
+++ 100-days-of-code-python/Day_006/task_2_while_loop.py
@@ -4,3 +4,8 @@ while number_of_hurdles > 0:
     print("Hurdle jumped!")
     number_of_hurdles -= 1
     print(f"{number_of_hurdles} hurdles remaining.")
+
+#while not at_goal:
+#    print("Keep running!") 
+#print("You have reached the goal!")
+
