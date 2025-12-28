80ae173b601b64bb9c9b27bbed5bf0d511fe2bcf
Sekander Dany <sekander.dany@reddotdigitalit.com>
2025-12-28 07:14:23 +0600

    adding contents

---
+++ 100-days-of-code-go/Day_001/task_003_naming_conventions.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	// PascalCase
+	// Eg CalculateArea, UserProfile, NewHTTPRequest
+	// Structs, interfaces, enums
+
+	// snake_case
+	// Eg user_id, first_name, total_amount
+
+	// UPPERCASE
+	// Use case is Constants
+
+	// mixedCase
+	// Eg javaScript, htmlDocument, isValid
+}
